f8dfad2390af2aa56e8abc70de7215260eb26044
LaisSBrito <89053153+LaisSBrito@users.noreply.github.com>
2021-11-15 13:30:29 -0300

    Update UsuarioJB.java

--- kidding-esv/kidding-esv/src/main/java/model/UsuarioJB.java
+++ kidding-esv/kidding-esv/src/main/java/model/UsuarioJB.java
@@ -1,45 +1,49 @@
 package model;
 
 public class UsuarioJB {
-
+	
+	private String Usuario_Id;
 	private String Usuario;
 	private String Email;
 	private String Senha;
-
-	public UsuarioJB(String usuario, String email, String senha) {
+	
+	
+	
+	public UsuarioJB(String usuario_Id, String usuario, String email, String senha) {
 		super();
+		Usuario_Id = usuario_Id;
 		Usuario = usuario;
 		Email = email;
 		Senha = senha;
 	}
-
 	public UsuarioJB() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
+	public String getUsuario_Id() {
+		return Usuario_Id;
+	}
+	public void setUsuario_Id(String usuario_Id) {
+		Usuario_Id = usuario_Id;
+	}
 	public String getUsuario() {
 		return Usuario;
 	}
-
 	public void setUsuario(String usuario) {
 		Usuario = usuario;
 	}
-
 	public String getEmail() {
 		return Email;
 	}
-
 	public void setEmail(String email) {
 		Email = email;
 	}
-
 	public String getSenha() {
 		return Senha;
 	}
-
 	public void setSenha(String senha) {
 		Senha = senha;
 	}
+	
 
 }
