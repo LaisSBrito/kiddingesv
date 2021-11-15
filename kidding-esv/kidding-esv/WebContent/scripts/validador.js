ea800f8c9010ac716a05129a8d0ecdce7e33e9af
LaisSBrito <89053153+LaisSBrito@users.noreply.github.com>
2021-11-15 13:31:53 -0300

    Update validador.js

--- kidding-esv/kidding-esv/WebContent/scripts/validador.js
+++ kidding-esv/kidding-esv/WebContent/scripts/validador.js
@@ -5,16 +5,33 @@
 function validar(){
 	let nome = cadastro.nome.value
 	let email = cadastro.email.value
+	let senha = cadastro.senha.value
+	let senha2 = cadastro.senha2.value
 	if (nome === "") {
-		alert('Preencha este campo')
+		alert('Preencha todos os campos')
 		cadastro.nome.focus()
 		return false
-	} else if (email === "") {
-		alert('Preencha este campo')
-		cadastro.nome.focus() 
+	}
+	 else if (email === "") {
+		alert('Preencha todos os campos') 
 		cadastro.email.focus()
 		return false
-	} else Document.forms["cadastro"].submit
+	} 
+	 else if (senha === "") {
+		alert('Preencha todos os campos') 
+		cadastro.senha.focus()
+		return false
+	} 
+	 else if (senha2 === "") {
+		alert('Preencha todos os campos') 
+		cadastro.senha2.focus()
+		return false			
+	} 
+	 if (senha !== senha2) {
+		alert('Senhas diferentes') 
+		cadastro.senha2.focus()
+		return false
+	}
+	 else { Document.forms["cadastro"].submit
+	}
 }
-
-
