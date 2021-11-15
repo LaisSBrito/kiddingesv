e72273833fb416cb7eb97a1e4a42c403c66599ce
LaisSBrito <89053153+LaisSBrito@users.noreply.github.com>
2021-11-15 13:29:07 -0300

    Update DAO.java

--- kidding-esv/kidding-esv/src/main/java/model/DAO.java
+++ kidding-esv/kidding-esv/src/main/java/model/DAO.java
@@ -2,6 +2,12 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
 
 public class DAO {
 
@@ -26,9 +32,8 @@ public class DAO {
 
 		}
 	}
-
 	// teste conexao
-	public void testeConexao() {
+	/**public void testeConexao() {
 		try {
 			Connection con = conectar();
 			System.out.println(con);
@@ -37,6 +42,58 @@ public class DAO {
 			System.out.println(e);
 		}
 
+	}**/
+	
+ /**CRUD USUARIO CREATE**/
+	
+	public void inserirUsuario(UsuarioJB user) {
+		String criar = "insert into USUARIO (Usuario,Email,Senha) values (?,?,?)";
+		try {
+			Connection con = conectar();
+			//preparar query execuçao no banco
+			PreparedStatement pst = con.prepareStatement(criar);
+			//substituir as (?)
+			pst.setString(1, user.getUsuario());
+			pst.setString(2, user.getEmail());
+			pst.setString(3, user.getSenha());
+			//Execução da query
+			pst.executeUpdate();
+			//encerrar a conexao
+			con.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
 	}
+	
+	
+	
+	
+	/** CRUD USUARIO LOGIN **/
+	
+	public ResultSet logarUsuario(UsuarioJB user) {
+		Connection con = conectar();
+		
+		try {
+			String logar = "select * from USUARIO where Email = ? and Senha = ?";
+		
+			PreparedStatement pst = con.prepareStatement(logar);
+			pst.setString(1, user.getEmail());
+			pst.setString(2, user.getSenha());
+			
+		
+			ResultSet rs = pst.executeQuery();
+			return rs;
+			
+		} catch (SQLException e) {
+			JOptionPane.showConfirmDialog(null,"dao: " + e);
+			return null;
+		}
+		
+		
 
+	}
+	
+}
 }
